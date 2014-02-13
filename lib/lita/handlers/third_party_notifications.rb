4c1c29ec5788932c3ccf04d6280884d12a1581ab
Marcel de Graaf <mail@marceldegraaf.net>
2014-02-13 13:26:28 +0100

    Updates

--- lib/lita/handlers/third_party_notifications.rb
+++ lib/lita/handlers/third_party_notifications.rb
@@ -4,16 +4,33 @@ module Lita
   module Handlers
     class ThirdPartyNotifications < Handler
 
+      #
+      # Always specify the token in the query parameters of the request
+      # e.g. http://lita.example.com/notifications/example?token=<token>
+      #
+
       http.post "/notifications/kitchen", :kitchen
 
       def self.default_config(config)
         config.channel = "#general"
+        config.enable_kitchen = true
       end
 
+      #
+      # Notifications from Kitchen
+      #
+      # Query params:
+      #   user        = user name
+      #   application = application name
+      #   message     = message
+      #
       def kitchen(request, response)
-        user        = query(:user)
-        application = query(:application)
-        message     = query(:message)
+        error(response, 401) and return unless authenticated?(request)
+        error(response, 404) and return unless enabled?(:kitchen)
+
+        user        = query(:user, request)
+        application = query(:application, request)
+        message     = query(:message, request)
 
         reply "[#{application}] #{message} (by #{user})"
       end
@@ -24,7 +41,7 @@ module Lita
         robot.send_messages(source, message)
       end
 
-      def query(field)
+      def query(field, request)
         request.POST[field.to_s]
       end
 
@@ -38,6 +55,18 @@ module Lita
 
       def config
         Lita.config.handlers.third_party_notifications
+      end
+
+      def authenticated?(request)
+        request.GET["token"] == config.token
+      end
+
+      def enabled?(name)
+        config.send("enable_#{name}") == true
+      end
+
+      def error(response, status)
+        response.status = status
       end
 
     end
--- spec/lita/handlers/kitchen_spec.rb
+++ spec/lita/handlers/kitchen_spec.rb
@@ -1,4 +1,7 @@
 require "spec_helper"
 
 describe Lita::Handlers::ThirdPartyNotifications, lita_handler: true do
+
+  it { routes_http(:post, "/notifications/kitchen").to(:kitchen) }
+
 end
